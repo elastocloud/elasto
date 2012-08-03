38078952219aa65954f683c51d01a19a62ad5dda
David Disseldorp <ddiss@samba.org>
2012-08-03 17:07:17 +0200

    license: add LGPL license headers
    Following a discussion with Ciaran Farrell from the SUSE legal team,
    add LGPL license headers to relevant source files. Such files are
    dual licensed LGPL-2.1 or LGPL-3.0.
    
    Base64 and ccan sources keep their existing licenses, BSD and LGPL-2.1+
    accordingly.

--- azure_xml.h
+++ azure_xml.h
@@ -1,7 +1,17 @@
 /*
- * Copyright (C) SUSE LINUX 2012, all rights reserved
+ * Copyright (C) SUSE LINUX Products GmbH 2012, all rights reserved.
  *
- * Author: ddiss@suse.de
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) version 3.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ *
+ * Author: David Disseldorp <ddiss@suse.de>
  */
 #ifndef _AZURE_XML_H_
 #define _AZURE_XML_H_
