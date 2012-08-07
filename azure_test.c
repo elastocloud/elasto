fb67d305f089d8a0d8660d31ed4d872cbd6a319b
David Disseldorp <ddiss@samba.org>
2012-08-07 11:54:48 +0200

    test: free subscription info when finished

--- azure_test.c
+++ azure_test.c
@@ -60,8 +60,7 @@ int main(void)
 
 	ret = azure_conn_init(pem_file, NULL, &aconn);
 	if (ret < 0) {
-		/* FIXME */
-		goto err_global_clean;
+		goto err_sub_info_free;
 	}
 
 	ret = azure_op_mgmt_get_sa_keys(sub_id, blob_acc, &op);
@@ -198,6 +197,10 @@ err_op_free:
 	azure_op_free(&op);
 err_conn_free:
 	azure_conn_free(&aconn);
+err_sub_info_free:
+	free(pem_file);
+	free(sub_id);
+	free(sub_name);
 err_global_clean:
 	azure_xml_subsys_deinit();
 	azure_conn_subsys_deinit();
