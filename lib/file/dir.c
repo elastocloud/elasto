8827fdb3163a3616c1149b6524b37d051a04c860
David Disseldorp <ddiss@suse.de>
2015-04-29 22:59:48 +0200

    lib/file/dir: expose S3 mkdir/rmdir support
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- lib/file/dir.c
+++ lib/file/dir.c
@@ -42,7 +42,8 @@ elasto_fmkdir(const struct elasto_fauth *auth,
 	int ret;
 	struct elasto_fh *fh;
 
-	if (auth->type != ELASTO_FILE_AZURE) {
+	if ((auth->type != ELASTO_FILE_AZURE)
+	 && (auth->type != ELASTO_FILE_S3)) {
 		ret = -ENOTSUP;
 		goto err_out;
 	}
@@ -72,7 +73,8 @@ elasto_frmdir(const struct elasto_fauth *auth,
 	int ret;
 	struct elasto_fh *fh;
 
-	if (auth->type != ELASTO_FILE_AZURE) {
+	if ((auth->type != ELASTO_FILE_AZURE)
+	 && (auth->type != ELASTO_FILE_S3)) {
 		ret = -ENOTSUP;
 		goto err_out;
 	}
