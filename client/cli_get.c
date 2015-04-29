02876b8add2389f3c9e83055d01d4ce1a3529da7
David Disseldorp <ddiss@suse.de>
2015-04-29 19:38:41 +0200

    cli/get: use libfile for blob and object retrieval
    libfile abstracts away most of the cloud provider specific behaviour, so
    can be used to retrieve Azure blob and S3 object paths with a much
    simpler implementation.
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- client/cli_get.c
+++ client/cli_get.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) SUSE LINUX Products GmbH 2012, all rights reserved.
+ * Copyright (C) SUSE LINUX GmbH 2012-2015, all rights reserved.
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
@@ -22,10 +22,12 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <inttypes.h>
 
 #include "ccan/list/list.h"
 #include "lib/exml.h"
 #include "lib/data_api.h"
+#include "lib/file/file_api.h"
 #include "lib/op.h"
 #include "lib/azure_blob_req.h"
 #include "lib/s3_req.h"
@@ -38,75 +40,8 @@
 void
 cli_get_args_free(struct cli_args *cli_args)
 {
+	free(cli_args->path);
 	free(cli_args->get.local_path);
-	if (cli_args->type == CLI_TYPE_AZURE) {
-		free(cli_args->az.blob_acc);
-		free(cli_args->az.ctnr_name);
-		free(cli_args->az.blob_name);
-	} else if (cli_args->type == CLI_TYPE_S3) {
-		free(cli_args->s3.bkt_name);
-		free(cli_args->s3.obj_name);
-	}
-}
-
-static int
-cli_get_az_args_parse(int argc,
-		      char * const *argv,
-		      struct cli_args *cli_args)
-{
-	int ret;
-
-	ret = cli_args_path_parse(cli_args->progname, cli_args->flags,
-				  argv[1],
-				  &cli_args->az.blob_acc,
-				  &cli_args->az.ctnr_name,
-				  &cli_args->az.blob_name);
-	if (ret < 0)
-		goto err_out;
-
-	if (cli_args->az.blob_name == NULL) {
-		cli_args_usage(cli_args->progname, cli_args->flags,
-		   "Invalid remote path, must be <account>/<container>/<blob>");
-		ret = -EINVAL;
-		goto err_args_free;
-	}
-
-	return 0;
-
-err_args_free:
-	free(cli_args->az.ctnr_name);
-	free(cli_args->az.blob_acc);
-err_out:
-	return ret;
-}
-
-static int
-cli_get_s3_args_parse(int argc,
-		      char * const *argv,
-		      struct cli_args *cli_args)
-{
-	int ret;
-
-	ret = cli_args_path_parse(cli_args->progname, cli_args->flags,
-				  argv[1],
-				  &cli_args->s3.bkt_name,
-				  &cli_args->s3.obj_name, NULL);
-	if (ret < 0)
-		goto err_out;
-
-	if (cli_args->s3.obj_name == NULL) {
-		cli_args_usage(cli_args->progname, cli_args->flags,
-		   "Invalid remote S3 path, must be <bucket>/<object>");
-		ret = -EINVAL;
-		goto err_bkt_free;
-	}
-
-	return 0;
-
-err_bkt_free:
-	free(cli_args->s3.bkt_name);
-err_out:
-	return ret;
 }
 
 int
@@ -116,171 +51,25 @@ cli_get_args_parse(int argc,
 {
 	int ret;
 
-	cli_args->get.local_path = strdup(argv[2]);
-	if (cli_args->get.local_path == NULL) {
+	/* path is parsed by libfile on open */
+	cli_args->path = strdup(argv[1]);
+	if (cli_args->path == NULL) {
 		ret = -ENOMEM;
 		goto err_out;
 	}
 
-	if (cli_args->type == CLI_TYPE_AZURE) {
-		ret = cli_get_az_args_parse(argc, argv, cli_args);
-	} else if (cli_args->type == CLI_TYPE_S3) {
-		ret = cli_get_s3_args_parse(argc, argv, cli_args);
-	} else {
-		ret = -ENOTSUP;
+	cli_args->get.local_path = strdup(argv[2]);
+	if (cli_args->get.local_path == NULL) {
+		ret = -ENOMEM;
+		goto err_path_free;
 	}
-	if (ret < 0) {
-		goto err_local_free;
-	}
+
 	cli_args->cmd = CLI_CMD_GET;
 
 	return 0;
 
-err_local_free:
-	free(cli_args->get.local_path);
-err_out:
-	return ret;
-}
-
-static int
-cli_get_blob_handle(struct cli_args *cli_args)
-{
-	struct elasto_conn *econn;
-	struct stat st;
-	struct op *op;
-	struct elasto_data *op_data;
-	int ret;
-
-	assert(cli_args->type == CLI_TYPE_AZURE);
-
-	ret = elasto_conn_init_az(cli_args->az.pem_file, NULL,
-				  cli_args->insecure_http, &econn);
-	if (ret < 0) {
-		goto err_out;
-	}
-
-	ret = cli_sign_conn_setup(econn,
-				  cli_args->az.blob_acc,
-				  cli_args->az.sub_id);
-	if (ret < 0) {
-		goto err_conn_free;
-	}
-
-	ret = stat(cli_args->get.local_path, &st);
-	if (ret == 0) {
-		printf("destination already exists at %s\n",
-		       cli_args->get.local_path);
-		goto err_conn_free;
-	}
-	printf("getting container %s blob %s for %s\n",
-	       cli_args->az.ctnr_name,
-	       cli_args->az.blob_name,
-	       cli_args->get.local_path);
-
-	ret = elasto_data_file_new(cli_args->get.local_path, 0, 0,
-				     O_CREAT | O_WRONLY,
-				     (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH),
-				     &op_data);
-	if (ret < 0) {
-		goto err_conn_free;
-	}
-
-
-	ret = az_req_blob_get(cli_args->az.blob_acc,
-				cli_args->az.ctnr_name,
-				cli_args->az.blob_name,
-				false,
-				op_data,
-				0, 0,
-				&op);
-	if (ret < 0) {
-		op_data->iov.buf = NULL;
-		elasto_data_free(op_data);
-		goto err_conn_free;
-	}
-
-	ret = elasto_conn_op_txrx(econn, op);
-	if (ret < 0) {
-		goto err_op_free;
-	}
-
-	/* TODO handle error */
-
-	ret = 0;
-err_op_free:
-	/* data buffer contains cli_args->get.local_path */
-	if (op->rsp.data)
-		op->rsp.data->iov.buf = NULL;
-	op_free(op);
-err_conn_free:
-	elasto_conn_free(econn);
-err_out:
-	return ret;
-}
-
-static int
-cli_get_obj_handle(struct cli_args *cli_args)
-{
-	struct elasto_conn *econn;
-	struct stat st;
-	struct op *op;
-	struct elasto_data *op_data;
-	int ret;
-
-	assert(cli_args->type == CLI_TYPE_S3);
-
-	ret = elasto_conn_init_s3(cli_args->s3.key_id,
-				  cli_args->s3.secret,
-				  cli_args->insecure_http, &econn);
-	if (ret < 0) {
-		goto err_out;
-	}
-
-	ret = stat(cli_args->get.local_path, &st);
-	if (ret == 0) {
-		printf("destination already exists at %s\n",
-		       cli_args->get.local_path);
-		goto err_conn_free;
-	}
-	printf("getting bucket %s container %s for %s\n",
-	       cli_args->s3.bkt_name,
-	       cli_args->s3.obj_name,
-	       cli_args->get.local_path);
-
-	ret = elasto_data_file_new(cli_args->get.local_path, 0, 0,
-				     O_CREAT | O_WRONLY,
-				     (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH),
-				     &op_data);
-	if (ret < 0) {
-		goto err_conn_free;
-	}
-
-	ret = s3_req_obj_get(cli_args->s3.bkt_name,
-			    cli_args->s3.obj_name,
-			    0, 0, /* entire obj */
-			    op_data,
-			    &op);
-	if (ret < 0) {
-		op_data->iov.buf = NULL;
-		elasto_data_free(op_data);
-		goto err_conn_free;
-	}
-
-	ret = elasto_conn_op_txrx(econn, op);
-	if (ret < 0) {
-		goto err_op_free;
-	}
-
-	/* TODO handle error */
-
-	ret = 0;
-err_op_free:
-	/* data buffer contains cli_args->get.local_path */
-	if (op->rsp.data)
-		op->rsp.data->iov.buf = NULL;
-	op_free(op);
-err_conn_free:
-	elasto_conn_free(econn);
+err_path_free:
+	free(cli_args->path);
 err_out:
 	return ret;
 }
@@ -288,11 +77,74 @@ err_out:
 int
 cli_get_handle(struct cli_args *cli_args)
 {
+	struct elasto_fh *fh;
+	struct elasto_fauth auth;
+	struct stat st;
+	struct elasto_fstat fstat;
+	struct elasto_data *dest_data;
+	int ret;
+
 	if (cli_args->type == CLI_TYPE_AZURE) {
-		return cli_get_blob_handle(cli_args);
+		auth.type = ELASTO_FILE_ABB;
+		auth.az.ps_path = cli_args->az.ps_file;
 	} else if (cli_args->type == CLI_TYPE_S3) {
-		return cli_get_obj_handle(cli_args);
+		auth.type = ELASTO_FILE_S3;
+		auth.s3.creds_path = cli_args->s3.creds_file;
+	} else {
+		ret = -ENOTSUP;
+		goto err_out;
+	}
+	auth.insecure_http = cli_args->insecure_http;
+
+	ret = stat(cli_args->get.local_path, &st);
+	if (ret == 0) {
+		printf("destination already exists at %s\n",
+		       cli_args->get.local_path);
+		ret = -EEXIST;
+		goto err_out;
 	}
 
-	return -ENOTSUP;
+	/* open without create or dir flags */
+	ret = elasto_fopen(&auth, cli_args->path, 0, NULL, &fh);
+	if (ret < 0) {
+		printf("%s path open failed with: %s\n",
+		       cli_args->path, strerror(-ret));
+		goto err_out;
+	}
+
+	/* stat to determine size to retrieve */
+	ret = elasto_fstat(fh, &fstat);
+	if (ret < 0) {
+		printf("stat failed with: %s\n", strerror(-ret));
+		goto err_fclose;
+	}
+
+	printf("getting %" PRIu64 " bytes from %s for %s\n",
+	       fstat.size, cli_args->path, cli_args->get.local_path);
+
+	ret = elasto_data_file_new(cli_args->get.local_path, 0, 0,
+				   O_CREAT | O_WRONLY,
+				   (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH),
+				   &dest_data);
+	if (ret < 0) {
+		goto err_fclose;
+	}
+
+	/* TODO implement and use seek(HOLE/DATA) here for efficiency */
+
+	ret = elasto_fread(fh, 0, fstat.size, dest_data);
+	if (ret < 0) {
+		printf("read failed with: %s\n", strerror(-ret));
+		goto err_fclose;
+	}
+
+	elasto_data_free(dest_data);
+
+	ret = 0;
+err_fclose:
+	if (elasto_fclose(fh) < 0) {
+		printf("close failed\n");
+	}
+err_out:
+	return ret;
 }
