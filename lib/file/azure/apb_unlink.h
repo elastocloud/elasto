66f4e73612f404ef8f4cfecde1f954f7595d0870
David Disseldorp <ddiss@suse.de>
2015-05-03 19:43:18 +0200

    lib/file: add Azure blob delete support
    In preperation for libfile elasto_funlink_close().
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>

---
+++ lib/file/azure/apb_unlink.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) SUSE LINUX GmbH 2015, all rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) version 3.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ */
+#ifndef _APB_UNLINK_H_
+#define _APB_UNLINK_H_
+
+int
+apb_funlink(void *mod_priv,
+	    struct elasto_conn *conn);
+
+#endif /* _APB_UNLINK_H_ */
