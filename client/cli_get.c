6e4bff28e60fdc7371ff5c97f71f17ccfd7c5733
David Disseldorp <ddiss@suse.de>
2016-02-20 18:25:24 +0100

    cli/get: perform space efficient downloads of sparse files
    For backends supporting sparse files, use the new elasto_flist_ranges()
    function to query for allocated ranges before proceeding with a
    download. Only pull down ranges which are allocated.
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- client/cli_get.c
+++ client/cli_get.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) SUSE LINUX GmbH 2012-2015, all rights reserved.
+ * Copyright (C) SUSE LINUX GmbH 2012-2016, all rights reserved.
  *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
@@ -24,6 +24,7 @@
 #include <fcntl.h>
 #include <inttypes.h>
 
+#include "ccan/list/list.h"
 #include "lib/file/file_api.h"
 #include "cli_common.h"
 #include "cli_get.h"
@@ -66,23 +67,34 @@ err_out:
 }
 
 struct cli_get_data_ctx {
+	struct elasto_fh *elasto_fh;
 	int fd;
 	char *path;
+	uint64_t total_len;
+	uint64_t num_ranges;
+	struct list_head ranges;
+};
+
+struct cli_get_range_ctx {
+	struct list_node list;
+	struct cli_get_data_ctx *data_ctx;
+	uint64_t off;
 	uint64_t len;
 };
 
 static int
-cli_get_data_in_cb(uint64_t stream_off,
-		   uint64_t got,
-		   uint8_t *in_buf,
-		   uint64_t buf_len,
-		   void *priv)
+cli_get_range_data_in_cb(uint64_t stream_off,
+			 uint64_t got,
+			 uint8_t *in_buf,
+			 uint64_t buf_len,
+			 void *priv)
 {
-	struct cli_get_data_ctx *data_ctx = priv;
+	struct cli_get_range_ctx *range_ctx = priv;
 	size_t wrote;
 	int ret;
 
-	wrote = pwrite(data_ctx->fd, in_buf, got, stream_off);
+	wrote = pwrite(range_ctx->data_ctx->fd, in_buf, got,
+		       range_ctx->off + stream_off);
 	if ((wrote == -1) || (wrote != got)) {
 		printf("write callback failed: %s\n", strerror(errno));
 		ret = -EBADF;
@@ -97,7 +109,58 @@ err_out:
 }
 
 static int
-cli_get_data_ctx_setup(const char *path,
+cli_get_range_ctx_setup(struct cli_get_data_ctx *data_ctx,
+			uint64_t range_off,
+			uint64_t range_len,
+			struct cli_get_range_ctx **_range_ctx)
+{
+	struct cli_get_range_ctx *range_ctx;
+
+	range_ctx = malloc(sizeof(*range_ctx));
+	if (range_ctx == NULL) {
+		return -ENOMEM;
+	}
+	memset(range_ctx, 0, sizeof(*range_ctx));
+	range_ctx->data_ctx = data_ctx;
+	range_ctx->off = range_off;
+	range_ctx->len = range_len;
+	list_add_tail(&data_ctx->ranges, &range_ctx->list);
+	data_ctx->num_ranges++;
+	*_range_ctx = range_ctx;
+
+	return 0;
+}
+
+static int
+cli_get_range_cb(struct elasto_frange *range,
+		 void *priv)
+{
+	struct cli_get_data_ctx *data_ctx = priv;
+	struct cli_get_range_ctx *range_ctx;
+	int ret;
+
+	ret = cli_get_range_ctx_setup(data_ctx, range->off, range->len,
+				      &range_ctx);
+	if (ret < 0) {
+		goto err_out;
+	}
+
+	ret = elasto_fread_cb(data_ctx->elasto_fh, range->off, range->len,
+			      range_ctx, cli_get_range_data_in_cb);
+	if (ret < 0) {
+		goto err_range_ctx_free;
+	}
+
+	return 0;
+err_range_ctx_free:
+	free(range_ctx);
+err_out:
+	return ret;
+}
+
+static int
+cli_get_data_ctx_setup(struct elasto_fh *fh,
+		       const char *path,
 		       uint64_t len,
 		       struct cli_get_data_ctx **_data_ctx)
 {
@@ -109,7 +172,9 @@ cli_get_data_ctx_setup(const char *path,
 		ret = -ENOMEM;
 		goto err_out;
 	}
+	memset(data_ctx, 0, sizeof(*data_ctx));
 
+	data_ctx->elasto_fh = fh;
 	data_ctx->fd = open(path, (O_CREAT | O_WRONLY),
 			    (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH));
 	if (data_ctx->fd == -1) {
@@ -131,7 +196,8 @@ cli_get_data_ctx_setup(const char *path,
 		ret = -ENOMEM;
 		goto err_fd_close;
 	}
-	data_ctx->len = len;
+	data_ctx->total_len = len;
+	list_head_init(&data_ctx->ranges);
 
 	*_data_ctx = data_ctx;
 
@@ -148,9 +214,15 @@ err_out:
 static void
 cli_get_data_ctx_free(struct cli_get_data_ctx *data_ctx)
 {
+	struct cli_get_range_ctx *range_ctx;
+	struct cli_get_range_ctx *range_ctx_n;
+
 	free(data_ctx->path);
 	if (close(data_ctx->fd) == -1) {
 		printf("close failed: %s\n", strerror(errno));
+	}
+	list_for_each_safe(&data_ctx->ranges, range_ctx, range_ctx_n, list) {
+		free(range_ctx);
 	}
 	free(data_ctx);
 }
@@ -159,6 +231,7 @@ int
 cli_get_handle(struct cli_args *cli_args)
 {
 	struct elasto_fh *fh;
+	struct elasto_fstatfs fstatfs;
 	struct stat st;
 	struct elasto_fstat fstat;
 	struct cli_get_data_ctx *data_ctx;
@@ -180,6 +253,13 @@ cli_get_handle(struct cli_args *cli_args)
 		goto err_out;
 	}
 
+	/* statfs to determine whether remote is sparse */
+	ret = elasto_fstatfs(fh, &fstatfs);
+	if (ret < 0) {
+		printf("fstatfs failed: %s\n", strerror(-ret));
+		goto err_fclose;
+	}
+
 	/* stat to determine size to retrieve */
 	ret = elasto_fstat(fh, &fstat);
 	if (ret < 0) {
@@ -190,18 +270,37 @@ cli_get_handle(struct cli_args *cli_args)
 	printf("getting %" PRIu64 " bytes from %s for %s\n",
 	       fstat.size, cli_args->path, cli_args->get.local_path);
 
-	ret = cli_get_data_ctx_setup(cli_args->get.local_path, fstat.size,
+	ret = cli_get_data_ctx_setup(fh, cli_args->get.local_path, fstat.size,
 				     &data_ctx);
 	if (ret < 0) {
 		goto err_fclose;
 	}
 
-	/* TODO implement and use seek(HOLE/DATA) here for efficiency */
+	if (fstatfs.cap_flags | ELASTO_FSTATFS_CAP_SPARSE) {
+		/*
+		 * perform space efficient download by only getting allocated
+		 * regions.
+		 */
+		ret = elasto_flist_ranges(fh, 0, fstat.size, 0,
+					  data_ctx, cli_get_range_cb);
+		if (ret < 0) {
+			printf("list ranges failed with: %s\n", strerror(-ret));
+			goto err_data_ctx_cleanup;
+		}
+	} else {
+		struct cli_get_range_ctx *range_ctx;
 
-	ret = elasto_fread_cb(fh, 0, fstat.size, data_ctx, cli_get_data_in_cb);
-	if (ret < 0) {
-		printf("read failed with: %s\n", strerror(-ret));
-		goto err_data_ctx_cleanup;
+		ret = cli_get_range_ctx_setup(data_ctx, 0, fstat.size,
+					      &range_ctx);
+		if (ret < 0) {
+			goto err_data_ctx_cleanup;
+		}
+
+		ret = elasto_fread_cb(fh, 0, fstat.size, range_ctx,
+				      cli_get_range_data_in_cb);
+		if (ret < 0) {
+			goto err_data_ctx_cleanup;
+		}
 	}
 
 	ret = 0;
