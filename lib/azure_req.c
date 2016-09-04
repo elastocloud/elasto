8a5739f19c965dd29dd2f3265b59c3b434db12ef
David Disseldorp <ddiss@suse.de>
2016-09-05 00:20:52 +0200

    lib/azure_req: bump x-ms-version to 2015-12-11
    2015-12-11 adds a new x-ms-server-encrypted response header, which may
    be useful in future.
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- lib/azure_req.c
+++ lib/azure_req.c
@@ -92,7 +92,7 @@ gen_date_str(void)
 }
 
 #define AZ_API_VERS_MGMT "2012-03-01"
-#define AZ_API_VERS_BLOB "2015-02-21"
+#define AZ_API_VERS_BLOB "2015-12-11"
 
 int
 az_req_common_hdr_fill(struct op *op,
