elastocloud/elasto

commit b9e832dd0d5b2f6d8ca3bc634be2ccc8c92450cb

lib: add new fopen_host() API
elasto_fopen_host() allows for callers to connect to a custom server,
rather than the default public cloud provider. Currently only the s3
backend supports this function.

Until now, libelasto has always used the following hosts for Amazon S3
requests:
- s3.amazonaws.com: for root / service requests
- <bkt>.s3.amazonaws.com: for non-root (bucket and object) requests

Although basic s3://<hostname> URIs have been supported for a while, the
hostname was never used.

Signed-off-by: David Disseldorp <ddiss@suse.de>

lib/s3_req.c
@@ -84,6 +84,39 @@ s3_req_sign(const char *acc,
 	return 0;
 }
 
+#if 0
+static int
+s3_req_v4_sign(const char *acc,
+	    const uint8_t *key,
+	    int key_len,
+	    struct op *op)
+{
+	int ret;
+	char *sig_str;
+	char *hdr_str;
+	struct s3_ebo *ebo = container_of(op, struct s3_ebo, op);
+
+	if (key == NULL) {
+		return -EINVAL;
+	}
+
+	ret = sign_s3v4_gen(ebo->req.path.bkt, key, key_len,
+			    op, &op->sig_src, &sig_str);
+	if (ret < 0) {
+		dbg(0, "S3 signing failed: %s\n",
+		    strerror(-ret));
+		return ret;
+	}
+
+	ret = op_req_hdr_add(op, "Authorization", sig_str);
+	if (ret < 0) {
+		return ret;
+	}
+
+	return 0;
+}
+#endif
+
 static void
 s3_req_free(struct op *op);
 static void
@@ -147,29 +180,72 @@ s3_req_fill_hdr_common(struct op *op)
 }
 
 int
-s3_req_hostname_get(char *bkt,
+s3_req_hostname_gen(const struct s3_path *path,
 		    char **_hostname)
 {
 	int ret;
 	char *hostname;
 
-	if (_hostname== NULL) {
+	if ((path == NULL) || (path->host == NULL) || (_hostname == NULL)) {
 		return -EINVAL;
 	}
 
-	if (bkt == NULL) {
-		hostname = strdup("s3.amazonaws.com");
-		if (hostname == NULL) {
+	if (path->bkt_as_host_prefix && path->bkt != NULL) {
+		ret = asprintf(&hostname, "%s.%s", path->bkt, path->host);
+		if (ret < 0) {
 			return -ENOMEM;
 		}
 	} else {
-		ret = asprintf(&hostname, "%s.s3.amazonaws.com", bkt);
-		if (ret < 0) {
+		hostname = strdup(path->host);
+		if (hostname == NULL) {
 			return -ENOMEM;
 		}
 	}
 
 	*_hostname = hostname;
+	return 0;
+}
+
+static int
+s3_req_url_path_gen(const struct s3_path *path,
+		    const char *url_params,
+		    char **_url_path)
+{
+	int ret;
+	const char *params_str = url_params ? url_params : "";
+	char *url_path;
+
+	switch (path->type) {
+	case S3_PATH_ROOT:
+		ret = asprintf(&url_path, "/%s", params_str);
+		break;
+	case S3_PATH_BKT:
+		if (path->bkt_as_host_prefix) {
+			ret = asprintf(&url_path, "/%s", params_str);
+			break;
+		}
+		ret = asprintf(&url_path, "/%s%s", path->bkt, params_str);
+		break;
+	case S3_PATH_OBJ:
+		if (path->bkt_as_host_prefix) {
+			ret = asprintf(&url_path, "/%s%s",
+				       path->obj, params_str);
+			break;
+		}
+		ret = asprintf(&url_path, "/%s/%s%s",
+			       path->bkt, path->obj, params_str);
+		break;
+	default:
+		dbg(0, "can't encode S3 path\n");
+		return -EINVAL;
+		break;
+	}
+	if (ret < 0) {
+		/* asprintf error */
+		return -errno;
+	}
+	*_url_path = url_path;
+
 	return 0;
 }
 
@@ -182,29 +258,14 @@ s3_req_url_encode(const struct s3_path *path,
 	int ret;
 	char *url_host;
 	char *url_path;
-	const char *params_str = url_params ? url_params : "";
 
-	ret = s3_req_hostname_get(path->bkt, &url_host);
+	ret = s3_req_hostname_gen(path, &url_host);
 	if (ret < 0) {
 		goto err_out;
 	}
 
-	if (S3_PATH_IS_SVC(path) || S3_PATH_IS_BKT(path)) {
-		/* svc and bkt requests use the same path, but host differs */
-		ret = asprintf(&url_path, "/%s", params_str);
-		if (ret < 0) {
-			ret = -ENOMEM;
-			goto err_uhost_free;
-		}
-	} else if (S3_PATH_IS_OBJ(path)) {
-		ret = asprintf(&url_path, "/%s%s", path->obj, params_str);
-		if (ret < 0) {
-			ret = -ENOMEM;
-			goto err_uhost_free;
-		}
-	} else {
-		dbg(0, "can't encode S3 path\n");
-		ret = -EINVAL;
+	ret = s3_req_url_path_gen(path, url_params, &url_path);
+	if (ret < 0) {
 		goto err_uhost_free;
 	}
 

lib/conn.c
@@ -755,14 +755,12 @@ elasto_conn_ev_connect(struct elasto_conn *econn)
 	struct evhttp_connection *ev_conn;
 	struct ssl_ctx_st *ssl_ctx;
 	struct ssl_st *ssl;
-	int port;
 
 	if ((econn->hostname == NULL) || (econn->ev_base == NULL)) {
 	       return -EINVAL;
 	}
 
 	if (econn->insecure_http) {
-		port = 80;
 		ev_bev = bufferevent_socket_new(econn->ev_base, -1,
 						BEV_OPT_CLOSE_ON_FREE);
 		if (ev_bev == NULL) {
@@ -771,7 +769,6 @@ elasto_conn_ev_connect(struct elasto_conn *econn)
 		}
 		dbg(1, "Using HTTP instead of HTTPS\n");
 	} else {
-		port = 443;
 		ret = elasto_conn_ev_ssl_init(econn, &ssl_ctx, &ssl);
 		if (ret < 0) {
 			goto err_out;
@@ -794,18 +791,24 @@ elasto_conn_ev_connect(struct elasto_conn *econn)
 		bufferevent_openssl_set_allow_dirty_shutdown(ev_bev, 1);
 	}
 
-	/* synchronously resolve hostname and connect */
+	/*
+	 * Address resolution and connection establishment occurs when the first
+	 * http request object is sent.
+	 */
 	ev_conn = evhttp_connection_base_bufferevent_new(econn->ev_base,
 							 NULL,
 							 ev_bev,
 							 econn->hostname,
-							 port);
+							 econn->port);
 	if (ev_conn == NULL) {
 		dbg(0, "failed to resolve/connect to %s:%d\n",
-		    econn->hostname, port);
+		    econn->hostname, econn->port);
 		ret = -EBADF;
 		goto err_bev_free;
 	}
+
+	dbg(1, "connected to %s:%d\n",
+	    econn->hostname, econn->port);
 
 	evhttp_connection_set_closecb(ev_conn, ev_conn_close_cb, econn);
 	/* 30s timeout */
@@ -1064,9 +1067,9 @@ elasto_conn_send_prepare(struct conn_op *conn_op,
 	uint64_t content_len = 0;
 	char *clen_str;
 
-	ret = asprintf(&url, "http%s://%s%s",
+	ret = asprintf(&url, "http%s://%s:%d%s",
 		       (econn->insecure_http ? "" : "s"),
-		       econn->hostname, op->url_path);
+		       econn->hostname, econn->port, op->url_path);
 	if (ret < 0) {
 		return -ENOMEM;
 	}
@@ -1126,6 +1129,10 @@ elasto_conn_send_prepare(struct conn_op *conn_op,
 		goto err_ev_req_free;
 	}
 
+	/*
+	 * FIXME: S3 differs, see:
+	 * http://docs.aws.amazon.com/AmazonS3/latest/dev/VirtualHosting.html
+	 */
 	ret = evhttp_add_header(ev_out_hdrs, "Host", op->url_host);
 	if (ret < 0) {
 		ret = -EINVAL;
@@ -1195,6 +1202,7 @@ elasto_conn_op_tx(struct elasto_conn *econn,
 		conn_op_flag_error(conn_op, -EINVAL);
 		goto err_out;
 	}
+	/* XXX assume matching port for now */
 
 	if (econn->insecure_http && op->url_https_only) {
 		dbg(0, "invalid connection for op 0x%x: connection is HTTP, "
@@ -1317,10 +1325,17 @@ static int
 elasto_conn_init_common(struct event_base *ev_base,
 			bool insecure_http,
 			const char *host,
+			uint16_t port,
 			struct elasto_conn **_econn)
 {
 	int ret;
-	struct elasto_conn *econn = malloc(sizeof(*econn));
+	struct elasto_conn *econn;
+
+	if (port < 1)  {
+		return -EINVAL;
+	}
+
+	econn = malloc(sizeof(*econn));
 	if (econn == NULL) {
 		ret = -ENOMEM;
 		goto err_out;
@@ -1334,6 +1349,7 @@ elasto_conn_init_common(struct event_base *ev_base,
 		ret = -ENOMEM;
 		goto err_conn_free;
 	}
+	econn->port = port;
 
 	*_econn = econn;
 
@@ -1355,7 +1371,8 @@ elasto_conn_init_az(struct event_base *ev_base,
 	struct elasto_conn *econn;
 	int ret;
 
-	ret = elasto_conn_init_common(ev_base, insecure_http, host, &econn);
+	ret = elasto_conn_init_common(ev_base, insecure_http, host,
+				      (insecure_http ? 80 : 443), &econn);
 	if (ret < 0) {
 		goto err_out;
 	}
@@ -1393,12 +1410,14 @@ elasto_conn_init_s3(struct event_base *ev_base,
 		    const char *secret,
 		    bool insecure_http,
 		    const char *host,
+		    uint16_t port,
 		    struct elasto_conn **econn_out)
 {
 	struct elasto_conn *econn;
 	int ret;
 
-	ret = elasto_conn_init_common(ev_base, insecure_http, host, &econn);
+	ret = elasto_conn_init_common(ev_base, insecure_http, host, port,
+				      &econn);
 	if (ret < 0) {
 		goto err_out;
 	}
@@ -1441,7 +1460,8 @@ elasto_conn_init_web(struct event_base *ev_base,
 	struct elasto_conn *econn;
 	int ret;
 
-	ret = elasto_conn_init_common(ev_base, insecure_http, host, &econn);
+	ret = elasto_conn_init_common(ev_base, insecure_http, host,
+				      (insecure_http ? 80 : 443), &econn);
 	if (ret < 0) {
 		goto err_out;
 	}

lib/conn.h
@@ -35,6 +35,7 @@ struct elasto_conn {
 	struct ssl_ctx_st *ssl_ctx;
 	struct ssl_st *ssl;
 	char *hostname;
+	uint16_t port;
 	struct {
 		char *account;
 		uint8_t *key;
@@ -77,6 +78,7 @@ elasto_conn_init_s3(struct event_base *ev_base,
 		    const char *secret,
 		    bool insecure_http,
 		    const char *host,
+		    uint16_t port,
 		    struct elasto_conn **econn);
 
 int

lib/file/amazon/s3_open.c
@@ -204,6 +204,8 @@ err_out:
 int
 s3_fopen(struct event_base *ev_base,
 	 void *mod_priv,
+	 const char *host,
+	 uint16_t port,
 	 const char *path,
 	 uint64_t flags,
 	 struct elasto_ftoken_list *open_toks)
@@ -212,19 +214,20 @@ s3_fopen(struct event_base *ev_base,
 	char *url_host;
 	struct s3_fh *s3_fh = mod_priv;
 
-	ret = s3_path_parse(path, &s3_fh->path);
+	ret = s3_path_parse(host, port, path, s3_fh->insecure_http,
+			    &s3_fh->path);
 	if (ret < 0) {
 		goto err_out;
 	}
 
-	/* TODO s3_fh->path.host is currently ignored */
-	ret = s3_req_hostname_get(s3_fh->path.bkt, &url_host);
+	ret = s3_req_hostname_gen(&s3_fh->path, &url_host);
 	if (ret < 0) {
 		goto err_path_free;
 	}
 
 	ret = elasto_conn_init_s3(ev_base, s3_fh->key_id, s3_fh->secret,
-				  s3_fh->insecure_http, url_host, &s3_fh->conn);
+				  s3_fh->insecure_http, url_host,
+				  s3_fh->path.port, &s3_fh->conn);
 	free(url_host);
 	if (ret < 0) {
 		goto err_path_free;

lib/file/amazon/s3_open.h
@@ -17,9 +17,11 @@
 int
 s3_fopen(struct event_base *ev_base,
 	 void *mod_priv,
+	 const char *host,
+	 uint16_t port,
 	 const char *path,
 	 uint64_t flags,
-	 struct elasto_ftoken_list *toks);
+	 struct elasto_ftoken_list *open_toks);
 
 int
 s3_fclose(void *mod_priv);

lib/file/azure/afs_open.c
@@ -527,12 +527,20 @@ err_out:
 int
 afs_fopen(struct event_base *ev_base,
 	  void *mod_priv,
+	  const char *host,
+	  uint16_t port,
 	  const char *path,
 	  uint64_t flags,
 	  struct elasto_ftoken_list *open_toks)
 {
 	int ret;
 	struct afs_fh *afs_fh = mod_priv;
+
+	if (host != NULL) {
+		dbg(0, "Azure backend currently missing custom host support\n");
+		ret = -EINVAL;
+		goto err_out;
+	}
 
 	ret = az_fs_path_parse(path, &afs_fh->path);
 	if (ret < 0) {

lib/file/azure/afs_open.h
@@ -17,6 +17,8 @@
 int
 afs_fopen(struct event_base *ev_base,
 	  void *mod_priv,
+	  const char *host,
+	  uint16_t port,
 	  const char *path,
 	  uint64_t flags,
 	  struct elasto_ftoken_list *toks);

lib/file/azure/apb_open.c
@@ -676,10 +676,17 @@ err_out:
 int
 apb_fopen(struct event_base *ev_base,
 	  void *mod_priv,
+	  const char *host,
+	  uint16_t port,
 	  const char *path,
 	  uint64_t flags,
 	  struct elasto_ftoken_list *open_toks)
 {
+	if (host != NULL) {
+		dbg(0, "Azure backend currently missing custom host support\n");
+		return -EINVAL;
+	}
+
 	return apb_abb_fopen(ev_base, mod_priv, path, flags, open_toks, true);
 }
 
@@ -699,9 +706,16 @@ apb_fclose(void *mod_priv)
 int
 abb_fopen(struct event_base *ev_base,
 	  void *mod_priv,
+	  const char *host,
+	  uint16_t port,
 	  const char *path,
 	  uint64_t flags,
 	  struct elasto_ftoken_list *open_toks)
 {
+	if (host != NULL) {
+		dbg(0, "Azure backend currently missing custom host support\n");
+		return -EINVAL;
+	}
+
 	return apb_abb_fopen(ev_base, mod_priv, path, flags, open_toks, false);
 }

commit cb8045232f69d78658cdc52256f080b9a33a9075

lib/s3_req: remove v4 signature stub code
b9e832dd0d5b2f6d8ca3bc634be2ccc8c92450cb accidentally added a stub
for v4 signature support. Remove it until the proper implementation
is done.

Signed-off-by: David Disseldorp <ddiss@suse.de>

lib/s3_req.c
@@ -84,39 +84,6 @@ s3_req_sign(const char *acc,
 	return 0;
 }
 
-#if 0
-static int
-s3_req_v4_sign(const char *acc,
-	    const uint8_t *key,
-	    int key_len,
-	    struct op *op)
-{
-	int ret;
-	char *sig_str;
-	char *hdr_str;
-	struct s3_ebo *ebo = container_of(op, struct s3_ebo, op);
-
-	if (key == NULL) {
-		return -EINVAL;
-	}
-
-	ret = sign_s3v4_gen(ebo->req.path.bkt, key, key_len,
-			    op, &op->sig_src, &sig_str);
-	if (ret < 0) {
-		dbg(0, "S3 signing failed: %s\n",
-		    strerror(-ret));
-		return ret;
-	}
-
-	ret = op_req_hdr_add(op, "Authorization", sig_str);
-	if (ret < 0) {
-		return ret;
-	}
-
-	return 0;
-}
-#endif
-
 static void
 s3_req_free(struct op *op);
 static void